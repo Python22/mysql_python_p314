b2f503edd61b260231e0d14600af81d3e11fe7db
admin <admin@admin.com>
2024-04-13 17:48:32 +0200

    conn

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import pymysql
 
 
 try:
-    
+    with pymysql.connect(host="localhost", port=3306, user="root", password="") as connection:
+        print(connection, "ok")
+        with connection.cursor() as cursor:
+            cursor.execute("""SHOW DATABASES""")
+            for i in cursor:
+                print(i)
+
+except pymysql.Error as e:
+    print(e)
